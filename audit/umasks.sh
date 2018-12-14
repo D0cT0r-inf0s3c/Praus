6b267a2cb885035eab921e5f54820d3cd62c890b
root <root@localhost.localdomain>
2018-12-14 08:02:09 -0500

    auditing

---
+++ audit/umasks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo
+echo -e "\e[1;95m-------------------------[umask audit in progress]-------------------------"
+
+umasklogin=$(grep -cP '^UMASK\s+077$' /etc/login.defs)
+if [ $umasklogin -eq 0 ];
+then
+  status="\e[91m[ BAD ]"
+  #exit
+else
+  status="\e[92m[ GOOD ]"
+fi
+echo -e "\e[39m[*] Checking if umask has been set for users\t\t\t\t\t\t$status"
+
+umasklogin=$(grep -cP '^umask\s+077$' /root/.bashrc)
+if [ $umasklogin -eq 0 ];
+then
+  status="\e[91m[ BAD ]"
+  #exit
+else
+  status="\e[92m[ GOOD ]"
+fi
+sleep 2
+echo -e "\e[39m[*] Checking if umask has been set for root\t\t\t\t\t\t$status"
+
+echo -e "\033[0m"
