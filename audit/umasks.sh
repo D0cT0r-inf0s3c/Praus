41f114ed7561f76455e4b44ce40c388e46b0996b
James Doctorinfosec <45049418+D0cT0r-inf0s3c@users.noreply.github.com>
2019-01-13 06:20:45 -0800

    Update umasks.sh

--- audit/umasks.sh
+++ audit/umasks.sh
@@ -15,7 +15,7 @@ then
 else
   status="\e[92m[ GOOD ]"
 fi
-echo -e "\e[39m[*] Checking if umask has been set for users\t\t\t\t\t\t$status"
+echo -e "\e[39m[*] Checking if umask has been set for users\t\t\t\t\t\t$status" > /$home/temp/umasks.txt
 
 umasklogin=$(grep -cP '^umask\s+077$' /root/.bashrc)
 if [ $umasklogin -eq 0 ];
@@ -26,6 +26,7 @@ else
   status="\e[92m[ GOOD ]"
 fi
 sleep 2
-echo -e "\e[39m[*] Checking if umask has been set for root\t\t\t\t\t\t$status"
+echo -e "\e[39m[*] Checking if umask has been set for root\t\t\t\t\t\t$status" >> > /$home/temp/umasks.txt
+
 
 echo -e "\033[0m"
